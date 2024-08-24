591cfa6ff09d70715abf1f0a11430f80e8f1836e
ossianhempel <hemposse@hotmail.com>
2024-08-24 11:35:21 +0200

    feat: simple file to upload files to minio (perhaps temporary)

---
+++ upload_to_minio.py
@@ -0,0 +1,12 @@
+from src.utils import upload_to_minio, connect_to_minio
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+MINIO_ENDPOINT = os.getenv('MINIO_ENDPOINT')
+MINIO_ACCESS_KEY = os.getenv('MINIO_ACCESS_KEY')
+MINIO_SECRET = os.getenv('MINIO_SECRET_KEY')
+
+client = connect_to_minio(endpoint=MINIO_ENDPOINT, access_key=MINIO_ACCESS_KEY, secret_key=MINIO_SECRET)
+upload_to_minio(client=client, file_path="merged_gw_20_21.csv", destination_bucket="gameweeks")
