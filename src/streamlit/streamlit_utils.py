4ea200f9ccdd7dc7cec0bc5a19b42dbbf8039eec
ossianhempel <hemposse@hotmail.com>
2024-09-14 12:48:16 +0200

    feat: create utils specific to streamlit dashboard

---
+++ src/streamlit/streamlit_utils.py
@@ -0,0 +1,40 @@
+
+import os
+import sys
+import streamlit as st
+import psycopg2
+
+# add the project root directory to the Python path
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
+sys.path.append(project_root)
+
+from src.utils import connect_to_postgres, query_postgres
+
+
+@st.cache_data
+def load_all_data(_connection, schema_name, table_name):
+    connection = _connection # tell streamlit to not cache connection
+    cursor = connection.cursor()
+    query = f"SELECT * FROM {schema_name}.{table_name}"
+    query_postgres(cursor, query)
+    data = cursor.fetchall()
+    column_names = [desc[0] for desc in cursor.description]
+    cursor.close()
+    connection.close()
+    return data, column_names
+
+@st.cache_resource
+def connect_to_postgres(database, host, user, password, port):
+    try:
+        connection = psycopg2.connect(
+            database=database,
+            host=host,
+            user=user,
+            password=password,
+            port=port
+        )
+        print('Connection to PG established, Connection object returned.')
+        return connection  # Return the connection object, not the cursor
+    except Exception as e:
+        print('Error: ', e)
+        return None
