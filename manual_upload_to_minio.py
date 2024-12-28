7dc185a3619a9681fbe94e0d9da322b70f9b452b
ossianhempel <hemposse@hotmail.com>
2024-12-28 21:55:58 +0100

    docs: add typing

--- manual_upload_to_minio.py
+++ manual_upload_to_minio.py
@@ -1,12 +1,12 @@
-from src.utils import upload_to_minio, connect_to_minio
+from src.utils.minio_utils import create_minio_client, upload_to_minio
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-MINIO_ENDPOINT = os.getenv('MINIO_ENDPOINT')
-MINIO_ACCESS_KEY = os.getenv('MINIO_ACCESS_KEY')
-MINIO_SECRET = os.getenv('MINIO_SECRET_KEY')
+MINIO_ENDPOINT = os.getenv('MINIO_ENDPOINT', 'localhost:9000')
+MINIO_ACCESS_KEY = os.getenv('MINIO_ACCESS_KEY', 'ossian')
+MINIO_SECRET = os.getenv('MINIO_SECRET_KEY', 'password')
 
 
 FILE_PATH = "src/data/fixtures_24_25.csv"
@@ -15,5 +15,9 @@ DESTINATION_BUCKET = "fixtures"
 assert FILE_PATH is not None, "FILE_PATH is not set"
 assert DESTINATION_BUCKET is not None, "DESTINATION_BUCKET is not set"
 
-client = connect_to_minio(endpoint=MINIO_ENDPOINT, access_key=MINIO_ACCESS_KEY, secret_key=MINIO_SECRET)
+client = create_minio_client(endpoint=MINIO_ENDPOINT, access_key=MINIO_ACCESS_KEY, secret_key=MINIO_SECRET)
+
+if client is None:
+    raise ValueError("Failed to create MinIO client")
+
 upload_to_minio(client=client, file_path=FILE_PATH, destination_bucket=DESTINATION_BUCKET)
