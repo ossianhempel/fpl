61ffaaef57949cf658fc66fa9fe462283e934783
ossianhempel <hemposse@hotmail.com>
2025-03-03 23:52:03 +0100

    chore: move to src/

--- src/manual_add_season_to_teams.py
+++ src/manual_add_season_to_teams.py
@@ -1,11 +1,12 @@
 import polars as pl
 
+
 def add_season_to_teams(file_path: str, season: str) -> pl.DataFrame:
     df = pl.read_csv(file_path)
     df = df.with_columns(pl.lit(season).alias("season"))
     return df
 
+
 if __name__ == "__main__":
     df = add_season_to_teams("src/data/teams_2024_25.csv", "2024-25")
     df.write_csv("src/data/teams_2024_25_with_season.csv")
-
--- src/manual_upload_to_minio.py
+++ src/manual_upload_to_minio.py
@@ -4,9 +4,9 @@ import os
 
 load_dotenv()
 
-MINIO_ENDPOINT = os.getenv('MINIO_ENDPOINT', 'localhost:9000')
-MINIO_ACCESS_KEY = os.getenv('MINIO_ACCESS_KEY', 'ossian')
-MINIO_SECRET = os.getenv('MINIO_SECRET_KEY', 'password')
+MINIO_ENDPOINT = os.getenv("MINIO_ENDPOINT", "localhost:9000")
+MINIO_ACCESS_KEY = os.getenv("MINIO_ACCESS_KEY", "ossian")
+MINIO_SECRET = os.getenv("MINIO_SECRET_KEY", "password")
 
 
 FILE_PATH = "src/data/fixtures_24_25.csv"
@@ -15,9 +15,13 @@ DESTINATION_BUCKET = "fixtures"
 assert FILE_PATH is not None, "FILE_PATH is not set"
 assert DESTINATION_BUCKET is not None, "DESTINATION_BUCKET is not set"
 
-client = create_minio_client(endpoint=MINIO_ENDPOINT, access_key=MINIO_ACCESS_KEY, secret_key=MINIO_SECRET)
+client = create_minio_client(
+    endpoint=MINIO_ENDPOINT, access_key=MINIO_ACCESS_KEY, secret_key=MINIO_SECRET
+)
 
 if client is None:
     raise ValueError("Failed to create MinIO client")
 
-upload_to_minio(client=client, file_path=FILE_PATH, destination_bucket=DESTINATION_BUCKET)
+upload_to_minio(
+    client=client, file_path=FILE_PATH, destination_bucket=DESTINATION_BUCKET
+)
