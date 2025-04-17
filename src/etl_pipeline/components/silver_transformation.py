a37329a26cbab267a81986e262a1d070ec23c494
ossianhempel <hemposse@hotmail.com>
2025-04-17 10:34:15 +0200

    feat: functions to fetch bronze data and validate expected cols, important cols, and key cols, including tests

---
+++ src/etl_pipeline/components/silver_transformation.py
@@ -0,0 +1,356 @@
+import pandas as pd
+from dotenv import load_dotenv
+from typing import Optional, Callable
+from minio import Minio
+import logging
+import os
+from dataclasses import dataclass
+
+from src.etl_pipeline.components.config import DataFetchConfig
+
+from src.utils.minio_utils import fetch_all_from_minio
+from src.utils.minio_utils import create_minio_client
+from src.config.logging_config import setup_logging
+
+
+# create logger for the module
+setup_logging()
+logger = logging.getLogger(__name__)
+
+# TODO: fetch from bronze layer and do minimal cleaning (EtLT) into silver layer as Parquet
+# TODO: cleaning and standardizing
+
+
+@dataclass
+class SilverTransformationConfig:
+    minio_endpoint: str = os.getenv("MINIO_ENDPOINT", "COULDNT GET ENV VAR")
+    minio_access_key: str = os.getenv("MINIO_ACCESS_KEY", "COULDNT GET ENV VAR")
+    minio_secret_key: str = os.getenv("MINIO_SECRET_KEY", "COULDNT GET ENV VAR")
+    source_bucket: str = "bronze"
+    destination_bucket: str = "silver"
+
+
+def fetch_bronze_data(
+    client: Minio,
+    fetch_function: Callable = fetch_all_from_minio,  # injecting util function
+    config: object = SilverTransformationConfig,
+) -> Optional[pd.DataFrame]:
+    """
+    Fetches all raw files from a given bucket in the bronze layer
+    """
+    logger.info(f"Initiating fetching of raw data from bucket: {config.source_bucket}")
+    try:
+        dfs = fetch_function(
+            client=client,
+            endpoint=config.minio_endpoint,
+            access_key=config.minio_access_key,
+            secret_key=config.minio_secret_key,
+            bucket_name=config.source_bucket,
+        )
+        if dfs is None or len(dfs) == 0:
+            logger.error("Fetch operation returned None instead of dataframes")
+            raise Exception("No data could fetched from gameweeks bucket")
+
+        logger.info(f"Number of gameweek dataframes fetched: {len(dfs)}")
+        return dfs
+    except Exception as e:
+        logger.error(f"Error occurred: {e}", exc_info=True)
+        raise Exception  # re-raise to handle in caller
+
+
+def validate_expected_columns(dataframe: pd.DataFrame, expected_columns: list) -> bool:
+    # TODO: what to do when actual df misses any cols from expected? fill with null/0 and have separate tests for crucial columns?
+    logger.info("Validating expected columns of dataframe")
+
+    # check that all expected cols are present in df
+    actual_cols = list(dataframe.columns)
+    sorted_actual_cols = sorted(actual_cols)
+    sorted_expected_cols = sorted(expected_columns)
+
+    if not sorted_actual_cols == sorted_expected_cols:
+        symmetric_difference = list(set(sorted_actual_cols) ^ set(sorted_expected_cols))
+        logger.info(
+            f"Expected: \n{sorted_expected_cols} \nReceived: \n{sorted_actual_cols}"
+        )
+        logger.info(f"\nColumns that are unique to each list: {symmetric_difference}")
+        # attempt to drop columns that aren't present in expected columns, then check again
+        try:
+            logger.info(
+                f"Dropping columns that aren't expected: {symmetric_difference}"
+            )
+            df = dataframe.copy()
+            df.drop(symmetric_difference, axis=1, inplace=True)
+            actual_cols = list(df.columns)
+            sorted_actual_cols = sorted(actual_cols)
+            symmetric_difference = list(
+                set(sorted_actual_cols) ^ set(sorted_expected_cols)
+            )
+
+            assert sorted_actual_cols == sorted_expected_cols
+        except Exception as e:
+            logger.error(f"Retried validation failed: {e}")
+
+            return False
+
+    # check that all cols in df are present in expected cols
+
+    logger.info("Successfully validated dataframe columns")
+    return True
+
+
+def validate_important_columns(dataframe: pd.DataFrame, key_columns: list) -> bool:
+    df = dataframe.copy()
+    columns = df.columns
+    # make sure required columns are not null
+    for col in columns:
+        if df[col].isnull().sum() > 0:
+            logger.error(f"{col} contains null values: {df[col].isnull().sum()}")
+            return False
+    logger.info("Validation of important columns completed")
+    return True
+
+
+def validate_key_columns(
+    dataframe: pd.DataFrame, key_columns: list, composite_key: bool = False
+) -> bool:
+    df = dataframe.copy()
+    logger.info("Validating key columns")
+    if composite_key:
+        assert len(key_columns) > 1, "Can't be a composite key with less than 2 columns"
+        key = key_columns
+        if df[key].duplicated().sum() > 0:
+            logger.error(f"Found duplicates in composite key: {key}")
+            return False
+
+    else:
+        columns = df[key_columns].columns
+        for col in columns:
+            if df[col].duplicated().sum() > 0:
+                logger.error(f"Found duplicates in individual key: {col}")
+                return False
+
+    logger.info("Validation of key columns successful")
+    return True
+
+
+def removes_dupes(dataframe: pd.DataFrame):
+    pass
+
+
+def remove_outliers(dataframe: pd.DataFrame) -> pd.DataFrame:
+    pass
+
+
+def test_sequential_values(dataframe: pd.DataFrame) -> bool:
+    pass
+
+
+def test_accepted_ranges(dataframe: pd.DataFrame) -> bool:
+    pass
+
+
+def merge_dataframes(dataframes: list[pd.DataFrame]) -> pd.DataFrame:
+    try:
+        if dataframes is not None:
+            combined_df = pd.concat(dataframes.values(), ignore_index=True)
+            logger.info(f"Combined gameweeks dataframe shape: {combined_df.shape}")
+            # logger.info("Convert pandas dataframe into polars dataframe")
+            # pd_df = pd.from_pandas(combined_df)
+        return combined_df
+    except Exception as e:
+        logger.error(f"Couldn't combine the fetched dataframes: {e}")
+        return None
+
+
+def transform_gameweeks():
+    pass
+
+
+def transform_fixtures():
+    pass
+
+
+def transform_teams():
+    pass
+
+
+def load_dataframe():
+    pass
+
+
+def fetch_gameweeks_from_lake(
+    client: Minio,
+    gw_fetcher_config: DataFetchConfig,
+    fetch_function: Callable = fetch_all_from_minio,  # inject util function
+) -> Optional[pd.DataFrame]:
+    """
+    Fetches all
+    """
+    logger.info("Initiating fetching of gameweeks")
+    try:
+        dfs = fetch_function(
+            client=client,
+            endpoint=gw_fetcher_config.minio.minio_endpoint,
+            access_key=gw_fetcher_config.minio.minio_access_key,
+            secret_key=gw_fetcher_config.minio.minio_secret_key,
+            bucket_name=gw_fetcher_config.bucket_name,
+        )
+        if dfs is None or len(dfs) == 0:
+            logger.error("Fetch operation returned None instead of dataframes")
+            raise Exception("No data could fetched from gameweeks bucket")
+
+        logger.info(f"Number of gameweek dataframes fetched: {len(dfs)}")
+
+    except Exception as e:
+        logger.error(f"Error occurred: {e}", exc_info=True)
+        raise  # re-raise to handle in caller
+
+    try:
+        if dfs is not None:
+            combined_df = pd.concat(dfs.values(), ignore_index=True)
+            logger.info(f"Combined gameweeks dataframe shape: {combined_df.shape}")
+        return combined_df
+    except Exception as e:
+        logger.error(f"Couldn't combine the fetched dataframes: {e}")
+        return None
+
+
+def fetch_fixtures_from_lake(
+    client: Minio, fixtures_fetcher_config: DataFetchConfig
+) -> Optional[pd.DataFrame]:
+    """ """
+    logger.info("Initiating fetching of fixtures")
+    try:
+        dfs = fetch_all_from_minio(
+            client=client,
+            endpoint=fixtures_fetcher_config.minio.minio_endpoint,
+            access_key=fixtures_fetcher_config.minio.minio_access_key,
+            secret_key=fixtures_fetcher_config.minio.minio_secret_key,
+            bucket_name=fixtures_fetcher_config.bucket_name,
+        )
+        if dfs is None or len(dfs) == 0:
+            logger.error("Fetch operation returned None instead of dataframes")
+            raise Exception("No data could fetched from fixtures bucket")
+
+        logger.info(f"Number of fixtures dataframes fetched: {len(dfs)}")
+    except Exception as e:
+        logger.error(f"Error occured: {e}", exc_info=True)
+
+    try:
+        if dfs is not None:
+            combined_df = pd.concat(dfs.values(), ignore_index=True)
+            logger.info(f"Combined fixtures dataframe shape: {combined_df.shape}")
+        return combined_df
+    except Exception as e:
+        logger.error(f"Couldn't combine the fetched dataframes: {e}")
+        return None
+
+
+def fetch_teams_from_lake(
+    client: Minio, teams_fetcher_config: DataFetchConfig
+) -> Optional[pd.DataFrame]:
+    """ """
+    logger.info("Initiating fetching of fixtures")
+    try:
+        dfs = fetch_all_from_minio(
+            client=client,
+            endpoint=teams_fetcher_config.minio.minio_endpoint,
+            access_key=teams_fetcher_config.minio.minio_access_key,
+            secret_key=teams_fetcher_config.minio.minio_secret_key,
+            bucket_name=teams_fetcher_config.bucket_name,
+        )
+        if dfs is None or len(dfs) == 0:
+            logger.error("Fetch operation returned None instead of dataframes")
+            raise Exception("No data could fetched from teams bucket")
+
+        logger.info(f"Number of teams dataframes fetched: {len(dfs)}")
+    except Exception as e:
+        logger.error(f"Error occured: {e}", exc_info=True)
+
+    try:
+        if dfs is not None:
+            combined_df = pd.concat(dfs.values(), ignore_index=True)
+            logger.info(f"Combined teams dataframe shape: {combined_df.shape}")
+        return combined_df
+    except Exception as e:
+        logger.error(f"Couldn't combine the fetched dataframes: {e}")
+        return None
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    client = create_minio_client(
+        endpoint=os.getenv("MINIO_ENDPOINT"),
+        access_key=os.getenv("MINIO_ACCESS_KEY"),
+        secret_key=os.getenv("MINIO_SECRET_KEY"),
+    )
+
+    config = SilverTransformationConfig()
+
+    dfs = fetch_bronze_data(
+        client=client,
+        fetch_function=fetch_all_from_minio,
+        config=SilverTransformationConfig,
+    )
+
+    expected_cols_gw = [
+        "assists",
+        "bonus",
+        "bps",
+        "clean_sheets",
+        "creativity",
+        "element",
+        "expected_assists",
+        "expected_goal_involvements",
+        "expected_goals",
+        "expected_goals_conceded",
+        "fixture",
+        "goals_conceded",
+        "goals_scored",
+        "ict_index",
+        "influence",
+        "ingestion_timestamp",
+        "kickoff_time",
+        "minutes",
+        "modified",
+        "name",
+        "opponent_team",
+        "own_goals",
+        "penalties_missed",
+        "penalties_saved",
+        "position",
+        "red_cards",
+        "round",
+        "saves",
+        "selected",
+        "starts",
+        "team",
+        "team_a_score",
+        "team_h_score",
+        "threat",
+        "total_points",
+        "transfers_balance",
+        "transfers_in",
+        "transfers_out",
+        "value",
+        "was_home",
+        "xP",
+        "yellow_cards",
+    ]
+    key_cols = [
+        "fixture",
+        "kickoff_time",
+        "name",
+        "team",
+        "opponent_team",
+        "total_points",
+        "position",
+    ]
+
+    for df in dfs.values():
+        assert validate_expected_columns(
+            df, expected_columns=expected_cols_gw
+        ), "Expected columns validation failed"
+        assert validate_important_columns(
+            df, key_columns=key_cols
+        ), "Key column validation failed"
---
+++ tests/etl/test_silver_transformation.py
@@ -0,0 +1,99 @@
+import pytest
+import pandas as pd
+
+from src.etl_pipeline.components.silver_transformation import (
+    validate_expected_columns,
+    validate_key_columns,
+)
+
+
+@pytest.fixture
+def fake_dataframe():
+    df = pd.DataFrame(
+        {
+            "date": [
+                "2024-03-01",
+                "2024-03-09",
+                "2024-05-10",
+                "2023-05-11",
+                "2024-03-14",
+            ],
+            "gw": [1, 2, 9, 23, 2],
+            "player_id": [1, 2, 3, 4, 1],
+            "season": ["2024-25", "2024-25", "2024-25", "2023-24", "2024-25"],
+            "score": [1, 3, 9, 3, 2],
+            "row_id": [1, 2, 3, 4, 5],
+        }
+    )
+
+    return df
+
+
+def test_validate_expected_columns(fake_dataframe):
+    expected_cols_1 = ["date", "gw", "season"]
+    assert (
+        validate_expected_columns(
+            dataframe=fake_dataframe, expected_columns=expected_cols_1
+        )
+        is True
+    )
+
+    expected_cols_2 = ["gw", "season", "column_not_in_df"]
+    assert (
+        validate_expected_columns(
+            dataframe=fake_dataframe, expected_columns=expected_cols_2
+        )
+        is False
+    )
+
+    expected_cols_3 = ["gw", "date", "season"]
+    assert (
+        validate_expected_columns(
+            dataframe=fake_dataframe, expected_columns=expected_cols_3
+        )
+        is True
+    )
+
+
+def test_validate_important_columns():
+    # TODO: this
+    pass
+
+
+def test_validate_key_columns(fake_dataframe):
+    key_cols_1 = ["date", "player_id"]
+    assert (
+        validate_key_columns(
+            dataframe=fake_dataframe, key_columns=key_cols_1, composite_key=True
+        )
+        is True
+    )
+
+    key_cols_2 = ["player_id", "gw"]
+    assert (
+        validate_key_columns(
+            dataframe=fake_dataframe, key_columns=key_cols_2, composite_key=True
+        )
+        is True
+    )
+
+    key_cols_3 = ["row_id"]
+    assert (
+        validate_key_columns(
+            dataframe=fake_dataframe, key_columns=key_cols_3, composite_key=False
+        )
+        is True
+    )
+
+    key_cols_4 = ["player_id"]
+    assert (
+        validate_key_columns(dataframe=fake_dataframe, key_columns=key_cols_4) is False
+    )
+
+
+def test_merge_dataframes():
+    pass
+
+
+def test_clean_dataframes():
+    pass
